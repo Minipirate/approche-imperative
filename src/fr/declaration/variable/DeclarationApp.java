4ecf1b6a14a7d96594e09650bce7cda47b6e9bcf
Minipirate <57010976+Minipirate@users.noreply.github.com>
2021-01-22 15:53:48 +0100

    v1

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package fr.declaration.variable;
+
+/**
+ * @author Ness'ti
+ *
+ */
+public class DeclarationApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		byte b = 30;
+		short s = 130;
+		int i = 330;
+		long l = 5000000000L;
+		float f = 3.0F;
+		double d = 30.5;
+		char c = 'c';
+		boolean bo = true;
+		String st = "coucou";
+		
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(c);
+		System.out.println(bo);
+		System.out.println(st);
+		
+		String randomString = "Voici le résultat d'un calcul:\n1+5=6";
+		
+		System.out.println(randomString);
+
+	}
+
+}
