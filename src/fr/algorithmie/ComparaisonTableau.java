d4fa5c93b6ab842c613fa31f504961bbdb8758a4
Minipirate <57010976+Minipirate@users.noreply.github.com>
2021-01-26 11:32:37 +0100

    Exercice 9 -> ComparaisonTableau

---
+++ src/fr/algorithmie/ComparaisonTableau.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package fr.algorithmie;
+
+/**
+ * @author Ness'ti
+ *
+ */
+public class ComparaisonTableau {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(
+				"------------- Affichage du nombre d'éléments en commun dans les deux tableaux ------------- ");
+
+		int[] array1 = { 1, 15, -3, 8, 7, 4, -2, 28, -1, 17, 2, 3, 0, 14, -4 };
+		int[] array2 = { 3, -8, 17, 5, -1, 4, 0, 6, 2, 11, -5, -4, 8 };
+
+		int total = 0;
+
+		for (int i = 0; i < array1.length; i++) {
+			for (int j = 0; j < array2.length; j++) {
+				if (array1[i] == array2[j]) {
+					total++;
+				}
+			}
+		}
+		System.out.print(total);
+	}
+}
